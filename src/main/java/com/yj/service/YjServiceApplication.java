0b24be0e1cca0005d223b6426c130f0e2bd762db
linxu.llx <linxu.llx@alibaba-inc.com>
2018-04-11 15:52:10 +0800

    feat: 项目搭建

---
+++ src/main/java/com/yj/service/YjServiceApplication.java
@@ -0,0 +1,12 @@
+package com.yj.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjServiceApplication.class, args);
+	}
+}
